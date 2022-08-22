3da2bde4c77ee45db4dd5baf1c74deb29e20cf90
adubatta <adubatta26@gmail.com>
2022-08-22 11:04:53 -0500

    8/22 Added Functionality

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -26,6 +26,49 @@ public class MainActivity extends AppCompatActivity {
 
         numberSumTV.setText("" + sum);
     }
+    public void findSub(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
 
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = num1 - num2;
+
+        numberSumTV.setText("" + sum);
+    }
+    public void findMult(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = num1 * num2;
+
+        numberSumTV.setText("" + sum);
+    }
+    public void findDiv(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = num1 / num2;
+
+        numberSumTV.setText("" + sum);
+    }
+    public void findPower(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = (num1 ^ num2);
+
+        numberSumTV.setText("" + sum);
+    }
 
 }
