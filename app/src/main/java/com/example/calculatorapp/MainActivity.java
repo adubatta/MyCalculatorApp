4cbd5f36c1e2dc2311cfee7b7a14dc4c6d1b492c
adubatta <adubatta26@gmail.com>
2022-08-23 11:46:20 -0500

    Added power and clear all functions

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.lang.Math;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -66,9 +69,39 @@ public class MainActivity extends AppCompatActivity {
 
         int num1 = Integer.parseInt((number1ET.getText().toString()));
         int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int sum = (num1 ^ num2);
-
+        int sum = (int) Math.pow(num1, num2);
         numberSumTV.setText("" + sum);
+    }
+    public void clearAll(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+        numberSumTV.setText("");
+        number1ET.setText("");
+        number2ET.setText("");
+    }
+    public void closeKeyboard()
+    {
+        // this will give us the view
+        // which is currently focus
+        // in this layout
+        View view = this.getCurrentFocus();
+
+        // if nothing is currently
+        // focus then this will protect
+        // the app from crash
+        if (view != null) {
+
+            // now assign the system
+            // service to InputMethodManager
+            InputMethodManager manager
+                    = (InputMethodManager)
+                    getSystemService(
+                            Context.INPUT_METHOD_SERVICE);
+            manager
+                    .hideSoftInputFromWindow(
+                            view.getWindowToken(), 0);
+        }
     }
 
 }
